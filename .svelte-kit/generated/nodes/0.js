10bcc7c7f47b7a773f25f2003ed20b97ef6da959
zen1tho <zen1tho.codes@gmail.com>
2022-12-14 19:21:38 +1100

    remove env and sveltekit files

--- .svelte-kit/generated/nodes/0.js
+++
@@ -1,4 +0,0 @@
-import * as shared from "../../../src/routes/+layout.ts";
-export { shared };
-export { default as component } from "../../../src/routes/+layout.svelte";
-export const server = true;
--- .svelte-kit/types/src/routes/proxy+layout.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import type { LayoutLoad } from './$types'
-import { getSupabase } from '@supabase/auth-helpers-sveltekit'
-
-export const load = async (event: Parameters<LayoutLoad>[0]) => {
-  const { session } = await getSupabase(event)
-  return { session }
-}
